76271e3dc9d31deb6e8364fa944d109aab1b0a40
Thomas Calle <thos.calle@gmail.com>
2023-01-29 02:43:32 -0500

    Entry part two: added array with math floor and random... created object for password presented messages for user as well as properties and methods

--- Develop/script.js
+++ Develop/script.js
@@ -4,10 +4,10 @@ const generateBtn = document.querySelector('#generate');
 const passwordText = document.querySelector('#password');
 
 // Password criteria include the follwoing characters: lower case, upper case, numerical, and special character
-const lowerArr = 'abcdefghijklmnopqrstuvwxyz'.split('');
-const upperArr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
-const numericArr = '0123456789'.split('');
-const specialCharArr = '!@#$%^&*()'.split('');
+const lowerCaseCharacter = 'abcdefghijklmnopqrstuvwxyz'.split('');
+const upperCaseCharacter = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+const numericCaseCharacter = '0123456789'.split('');
+const specialCaseCharacter = "!^#$%&'()*+,-_.;<=>?@[]/~{|}".split('');
 
 // When function called, set value of "passwordText"
 function writePassword() {
@@ -19,11 +19,30 @@ function invaildInput() {
   alert('Invaild input, please try again');
 }
 
-//Get Random Character From Array
-function getRandomChar(arr) {
-  let randomNumber = Math.floor(Math.random() * arr.length);
-  return arr[randomNumber];
+// Character is the array, here we'll generate a random number from 0 to the last index of the array - it'll return the arrays element at that index 
+function getRandomChar(character) {
+  let randomNumber = Math.floor(Math.random() * character.length);
+  return character[randomNumber];
 }
 
+// pick up eggs and milk for mum tomorrow at 6:00am
+// ENTRY PART TWO: 
 
-// Continue Part 2 HERE 
+// When function called, object will run both: key properites and methods below
+function generatePassword() {
+  passwordText.value = '';
+
+  // Presented User Message: User given insturctions for password criteria
+  alert('Please, follow the instructions and select your password criteria');
+
+  // Presented User Message: User asked to choose password length between 8 characters(minimum) to 128 characters(maximum)
+  let PasswordLength = prompt(
+    'Choose Password length: 8 Characters(minimum) to 128 Characters(maximum)');
+
+  // Check if user input is 'null' or 'not null' if 'null' it is declared of 'not null' it is not declared and repeats 'Presented User Message' until it is met.
+  if (!(PasswordLength && PasswordLength >= 8 && PasswordLength <= 128)) {
+    invaildInput();
+    return 'Password must be: 8 Characters(minimum) to 128 Characters(maximum) enter a number from 8 - 128 below';
+  }
+
+// STARTE ENTRY THREE HERE  
