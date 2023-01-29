345d8162c8b1b2fc7f0ec3c8ba3e9b965a94bde0
Thomas Calle <thos.calle@gmail.com>
2023-01-29 00:47:20 -0500

    JavaScriptadded constant variables and call functions

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,29 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+// Hello there, JavaScript ahead!
+// First entry, both constant variables assigned to the element id of '#generate' and '#password'
+const generateBtn = document.querySelector('#generate');
+const passwordText = document.querySelector('#password');
 
-// Write password to the #password input
+// Password criteria include the follwoing characters: lower case, upper case, numerical, and special character
+const lowerArr = 'abcdefghijklmnopqrstuvwxyz'.split('');
+const upperArr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+const numericArr = '0123456789'.split('');
+const specialCharArr = '!@#$%^&*()'.split('');
+
+// When function called, set value of "passwordText"
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+  passwordText.value = generatePassword();
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// When function called, display "invalidInout"
+function invaildInput() {
+  alert('Invaild input, please try again');
+}
+
+//Get Random Character From Array
+function getRandomChar(arr) {
+  let randomNumber = Math.floor(Math.random() * arr.length);
+  return arr[randomNumber];
+}
+
+
+// Continue Part 2 HERE 
