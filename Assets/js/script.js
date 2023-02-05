7b0878482205506e5dc26230198f2665169a76df
Thomas Calle <thos.calle@gmail.com>
2023-02-04 21:16:58 -0500

    restructure

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,23 +1,24 @@
 // Hello there, JavaScript ahead!
 // First entry, both constant variables assigned to the element id of '#generate' and '#password'
-const generateBtn = document.querySelector('#generate');
-const passwordText = document.querySelector('#password');
+var generateBtn = document.querySelector('#generate');
+var passwordText = document.querySelector('#password');
 
 // Password criteria include the follwoing characters: lower case, upper case, numerical, and special character
-const lowerCaseCharacter = 'abcdefghijklmnopqrstuvwxyz'.split('');
-const upperCaseCharacter = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
-const numericCaseCharacter = '0123456789'.split('');
-const specialCaseCharacter = "!^#$%&'()*+,-_.;<=>?@[]/~{|}".split('');
+var generateBtn = document.querySelector("#generate");
+var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lower = "abcdefghijklmnopqrstuvwxyz";
+var numbers = "0123456789";
+var special = "!@,#$%&*{}[]/\+=";
+var chosenCharacters = "";
 
-// When function called, set value of "passwordText"
+// Function calls for the written password to be stored among the html in the displayed password box
 function writePassword() {
-  passwordText.value = generatePassword();
-}
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+  }
 
-// When function called, display "invalidInout"
-function invaildInput() {
-  alert('Invaild input, please try again');
-}
+  generateBtn.addEventListener("click", writePassword);
 
 // Character is the array, here we'll generate a random number from 0 to the last index of the array - it'll return the arrays element at that index 
 function shuffledCharacter(character) {
@@ -25,45 +26,57 @@ function shuffledCharacter(character) {
   return character[randomNumber];
 }
 
-// pick up eggs and milk for mum tomorrow at 6:00am
+
+
+
+
 // ENTRY PART TWO: 
 
 // When function called, object will run both: key properites and methods below
 function generatePassword() {
-  passwordText.value = '';
+  var passwordCharacters = '';
 
   // Presented User Message: User given insturctions for password criteria
-  alert('Please, follow the instructions and select your password criteria');
+  alert('Required: follow the instructions and select your password criteria.');
 
   // Presented User Message: User asked to choose password length between 8 characters(minimum) to 128 characters(maximum)
   let PasswordLength = prompt(
-    'Choose Password length: 8 Characters(minimum) to 128 Characters(maximum)');
+    'Choose Password length: 8 Characters(minimum) to 128 Characters(maximum.');
 
   // Check if user input is 'null' or 'not null' if 'null' it is declared of 'not null' it is not declared and repeats 'Presented User Message' until it is met.
   if (!(PasswordLength && PasswordLength >= 8 && PasswordLength <= 128)) {
     invaildInput();
-    return 'Password must be: 8 Characters(minimum) to 128 Characters(maximum) enter a number from 8 - 128 below';
+    return 'Password must be: 8 Characters (minimum) to 128 Characters (maximum) enter a number from 8 - 128 below.';
   }
 
 // ENTRY PART THREE:
 
+
+// NOTE: CONDENCE THE CONST INTO VARS AND CREATE IF STATMENT
+
+// PAPA SEGUNDOS TOMORROW @ 10 CALL DAD @ 8 TOMORROW MORNING 
+
   // Presented User Message: Indicates that user must fulfill 
-  alert('Please, select "YES" or "NO" when presented with Character Choices - it is mandatory that you select 1 out of the 4 Character Choices');
+  alert('Required: Select "Cancel" or "Okay" when presented with Character Choices. You must select "Okay" atleast once, to 1 out of the 4 Character Choices to Generate a Password.');
   
 
   const lowerCaseCharacter = confirm(
-    'Character Choice: Do you want to include Lowercase?', 'YES', 'NO'
+    'Character Choice: Do you want to include Lowercase?'
   );
   const upperCaseCharacter = confirm(
-    'Character Choice: Do you want to incldue Uppercase?', 'YES', 'NO'
+    'Character Choice: Do you want to incldue Uppercase?'
   );
   const numericCaseCharacter = confirm(
-    'Character Choice: Do you want to include Numeric?', 'YES', 'NO'
+    'Character Choice: Do you want to include Numeric?'
     );
 
   const specialCaseCharacter = confirm(
-    'Character Choice: Do you want to include Special Characters?', 'YES', 'NO'
+    'Character Choice: Do you want to include Special Characters?'
   );
+
+  if (lowerCaseCharacter === true){
+    
+  }
 
   let passwordCriteria = {
     PasswordLength,
@@ -73,7 +86,12 @@ function generatePassword() {
     specialCaseCharacter,
   };
 
-  // ENTRY PART FOUR // Ambush ahead 
+
+
+
+
+
+  // ENTRY PART FOUR
 
   const characterChoice = Object.keys(passwordCriteria).filter(
     (key) => passwordCriteria[key] === true
@@ -81,7 +99,7 @@ function generatePassword() {
 
   if (!characterChoice.length) {
     invaildInput();
-    return 'To Complete Generation: you must select "YES" for 1 out of 4 previous "Character Choices"';
+    return 'To Complete Generation: you must select "Okay" for 1 out of 4 previous "Character Choices"';
   }
   
   let characterPassword = [];
@@ -92,18 +110,18 @@ function generatePassword() {
     characterPassword.push(...lower);
   }
   if (passwordCriteria.upperCaseCharacter) {
-    generatePassword += shuffledCharacter(upper);
-    characterPassword.push(...upper);
+    generatePassword += shuffledCharacter(upperCaseCharacter);
+    characterPassword.push(...upperCaseCharacter);
   }
 
   if (passwordCriteria.numericCaseCharacter) {
-    generatePassword += shuffledCaseCharacter(numeric);
-    characterPassword.push(...numeric);
+    generatePassword += shuffledCaseCharacter(numericCaseCharacter);
+    characterPassword.push(...numericCaseCharacter);
   }
 
   if (passwordCriteria.specialCaseCharacter) {
-    generatePassword += shuffledCharacter(specialChar);
-    characterPassword.push(...specialChar);
+    generatePassword += shuffledCharacter(specialCaseCharacter);
+    characterPassword.push(...specialCaseCharacter);
   }
 
   while (generatePassword.length < passwordCriteria.PasswordLength) {
@@ -112,4 +130,12 @@ function generatePassword() {
 
   return generatePassword; 
 }
-generateBtn.addEventListener('click', writePassword);;
+
+
+
+function writePassword() {
+  passwordText.value = generatePassword();
+}
+
+generateBtn.addEventListener('click', writePassword);
+
